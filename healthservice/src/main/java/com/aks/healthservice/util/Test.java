07460539a327af401835d44009e2ddc5c0c3c1bb
commercial4sam <commercial4sam@gmail.com>
2018-09-16 16:27:38 -0400

    test commit 1

--- healthservice/src/main/java/com/aks/healthservice/util/Test.java
+++ healthservice/src/main/java/com/aks/healthservice/util/Test.java
@@ -12,11 +12,14 @@ public class Test {
 
 		System.out.println("Testing branch checkout --->");
 		
+		System.out.println("Git Checkout switch branch to master");
 		int i=5;
 		System.out.println(++i);
 		
 		int j=5;
 		System.out.println(j++);
+		
+		System.out.println(" Branching and merging ");
 	
 	}
 	
