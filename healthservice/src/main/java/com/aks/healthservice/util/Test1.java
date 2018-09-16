b8a0eb62e23478476effdc1715198b103aae2c44
commercial4sam <commercial4sam@gmail.com>
2018-09-16 16:32:54 -0400

    test commit 2

--- healthservice/src/main/java/com/aks/healthservice/util/Test1.java
+++ healthservice/src/main/java/com/aks/healthservice/util/Test1.java
@@ -2,10 +2,11 @@ package com.aks.healthservice.util;
 
 public class Test1 {
 	
+	
 	public static final Test1 test1=new Test1();
 	
 	private Test1(){
-		
+	System.out.println("Branching and Merging.....");	
 	}
 
 	
